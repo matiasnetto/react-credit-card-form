9fe5d654c0dbc3a089e856c1ab47395802b32c02
Matias Netto <matiasnetto03@gmail.com>
2022-10-20 17:17:12 -0300

    add open graph

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: './public',
 });
+
+/*
+test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__test__/setup.ts',
+  },
+
+*/
